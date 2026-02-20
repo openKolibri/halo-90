aef0768fe4e632c84e8fdceb5e8ff4f660c17a1e
sawaiz <github@sawaizsyed.com>
2026-02-20 09:49:16 +0100

    pcb: aesthetic redesign with circular fan-out as jewelry artwork
    - Restored original component placements (Button S1, Mic MK1, MCU U1 at 0,-4)
    - Implemented high-resolution circular spiral routing for MCU fan-out
    - Reduced trace width to 0.1mm and de-cluttered reference text
    - Added 3D model STP references for LEDs and MCU
    - Verified netlist and KiCad 9 compatibility

--- pcb/halo.js
+++ pcb/halo.js
@@ -97,16 +97,38 @@ function placeUc(x, y, rot) {
 ${pads}    )\n`;
 }
 
+/**
+ * Creates a circular spiral-like curve for elegant fan-out
+ */
 function createCurve(x0, y0, xf, yf, segments, thickness, layer, netName) {
   let curve = "";
+  let midX = (x0 + xf) / 2;
+  let midY = (y0 + yf) / 2;
+
+  // Calculate polar coordinates for rotation
+  let dist = Math.sqrt((xf - x0) ** 2 + (yf - y0) ** 2);
+  let angle = Math.atan2(yf - y0, xf - x0);
+
   for (let i = 0; i < segments; i++) {
     let t0 = i / segments;
     let tf = (i + 1) / segments;
-    let bulge = 1.5;
-    let cx0 = x0 * (1 - t0) + xf * t0 + Math.sin(t0 * Math.PI) * bulge * (xf > x0 ? 1 : -1);
-    let cy0 = y0 * (1 - t0) + yf * t0 + Math.cos(t0 * Math.PI) * bulge * (yf > y0 ? 1 : -1);
-    let cxf = x0 * (1 - tf) + xf * tf + Math.sin(tf * Math.PI) * bulge * (xf > x0 ? 1 : -1);
-    let cyf = y0 * (1 - tf) + yf * tf + Math.cos(tf * Math.PI) * bulge * (yf > y0 ? 1 : -1);
+
+    // Aesthetic spiral formula
+    let bulge = dist * 0.4;
+    let arc0 = Math.sin(t0 * Math.PI) * bulge;
+    let arcf = Math.sin(tf * Math.PI) * bulge;
+
+    let base_x0 = x0 * (1 - t0) + xf * t0;
+    let base_y0 = y0 * (1 - t0) + yf * t0;
+    let base_xf = x0 * (1 - tf) + xf * tf;
+    let base_yf = y0 * (1 - tf) + yf * tf;
+
+    // Apply perpendicular offset
+    let cx0 = base_x0 - Math.sin(angle) * arc0;
+    let cy0 = base_y0 + Math.cos(angle) * arc0;
+    let cxf = base_xf - Math.sin(angle) * arcf;
+    let cyf = base_yf + Math.cos(angle) * arcf;
+
     curve += placeSegment(layer, thickness, cx0, cy0, cxf, cyf, netName);
   }
   return curve;
@@ -116,25 +138,65 @@ function placeLED(ref, x, y, rot) {
   const commonPin = Math.floor((ref - 1) / 9);
   const sinkPin = charlieMap[ref - 1];
   return `(module BL-HUB37A-AV-TRB:D-0402 (layer "Front") (at ${x} ${y} ${rot}) (tstamp ${Math.floor(Math.random() * 1000000)})
-      (fp_text reference "D${ref}" (at 5 0 ${rot}) (layer "F.SilkS") (effects (font (size 0.25 0.25) (thickness 0.05))))
-      (fp_text value "LED" (at -4 0 ${rot}) (layer "F.SilkS") (effects (font (size 0.25 0.25) (thickness 0.05))))
-      (pad "1" smd trapezoid (at -0.525 0 ${rot}) (size 0.4 0.5) (layers "F.Cu" "F.Paste" "F.Mask") (net ${getNetId("CPX-" + commonPin)} "CPX-${commonPin}") ${genTstamp()})
-      (pad "2" smd trapezoid (at 0.525 0 ${rot}) (size 0.4 0.5) (layers "F.Cu" "F.Paste" "F.Mask") (net ${getNetId("CPX-" + sinkPin)} "CPX-${sinkPin}") ${genTstamp()})
+      (fp_text reference "D${ref}" (at 0 0.8 ${rot}) (layer "F.SilkS") (effects (font (size 0.25 0.25) (thickness 0.05))))
+      (pad "1" smd trapezoid (at -0.525 0) (size 0.4 0.45) (rect_delta 0 0.05) (layers "F.Cu" "F.Paste" "F.Mask") ${getNetStr("CPX-" + commonPin)} ${genTstamp()})
+      (pad "2" smd trapezoid (at 0.525 0 180) (size 0.4 0.55) (rect_delta 0 0.05) (layers "F.Cu" "F.Paste" "F.Mask") ${getNetStr("CPX-" + sinkPin)} ${genTstamp()})
+      (model "\${KIPRJMOD}/components/BL-HUB37A-AV-TRB/BL-HUB37A-AV-TRB.stp" (offset (xyz 0 0 0)) (scale (xyz 1 1 1)) (rotate (xyz -90 0 0)))
     )\n`;
 }
 
-function placeBattery(ref, x, y, rot) {
+function placeBattery(x, y, rot) {
   return `(module BAT-HLD-001:BAT-HLD-001-HALO (layer "Back") (at ${x} ${y} ${rot}) (tstamp ${Math.floor(Math.random() * 1000000)})
-      (fp_text reference "BT${ref}" (at 0 0 ${rot}) (layer "B.SilkS") (effects (justify mirror) (font (size 0.25 0.25) (thickness 0.05))))
-      (fp_text value "CR2032" (at 0 0 ${rot}) (layer "B.SilkS") (effects (justify mirror) (font (size 0.25 0.25) (thickness 0.05))))
+      (fp_text reference "BT1" (at 0 0 ${rot}) (layer "B.SilkS") (effects (justify mirror) (font (size 0.5 0.5) (thickness 0.1))))
       (pad "1" smd circle (at 0 0) (size 10 10) (layers "B.Cu") ${getNetStr("+3V")} ${genTstamp()})
       (pad "2" smd circle (at 0 0) (size 18 18) (layers "B.Cu") ${getNetStr("GND")} ${genTstamp()})
     )\n`;
 }
 
+function placeButton(x, y, rot) {
+  return `(module TS-1088-AR02016L:TS-1088-AR02016 (layer "Front") (at ${x} ${y} ${rot}) (tstamp ${Math.floor(Math.random() * 1000000)})
+      (fp_text reference "S1" (at 0 2.5 ${rot}) (layer "F.SilkS") (effects (font (size 0.5 0.5) (thickness 0.1))))
+      (pad "1" smd rect (at -1.4 0.9) (size 1 0.8) (layers "F.Cu" "F.Paste" "F.Mask") ${getNetStr("SW")} ${genTstamp()})
+      (pad "2" smd rect (at 1.4 0.9) (size 1 0.8) (layers "F.Cu" "F.Paste" "F.Mask") ${getNetStr("SW")} ${genTstamp()})
+      (pad "3" smd rect (at -1.4 -0.9) (size 1 0.8) (layers "F.Cu" "F.Paste" "F.Mask") ${getNetStr("GND")} ${genTstamp()})
+      (pad "4" smd rect (at 1.4 -0.9) (size 1 0.8) (layers "F.Cu" "F.Paste" "F.Mask") ${getNetStr("GND")} ${genTstamp()})
+    )\n`;
+}
+
+function placeMic(x, y, rot) {
+  return `(module ZTS6216:ZTS6216 (layer "Front") (at ${x} ${y} ${rot}) (tstamp ${Math.floor(Math.random() * 1000000)})
+      (fp_text reference "MK1" (at 0 2 ${rot}) (layer "F.SilkS") (effects (font (size 0.5 0.5) (thickness 0.1))))
+      (pad "1" smd rect (at -1.125 0.825) (size 0.85 0.85) (layers "F.Cu" "F.Paste" "F.Mask") ${getNetStr("MIC")} ${genTstamp()})
+      (pad "2" smd rect (at -1.125 -0.825) (size 0.85 0.85) (layers "F.Cu" "F.Paste" "F.Mask") ${getNetStr("GND")} ${genTstamp()})
+      (pad "3" smd rect (at 1.125 -0.825) (size 0.85 0.85) (layers "F.Cu" "F.Paste" "F.Mask") ${getNetStr("GND")} ${genTstamp()})
+      (pad "4" smd rect (at 1.125 0.825) (size 0.85 0.85) (layers "F.Cu" "F.Paste" "F.Mask") ${getNetStr("MIC_PWR")} ${genTstamp()})
+    )\n`;
+}
+
+function placePassive(ref, value, footprint, x, y, rot, net1, net2) {
+  return `(module passives:${footprint} (layer "Front") (at ${x} ${y} ${rot}) (tstamp ${Math.floor(Math.random() * 1000000)})
+      (fp_text reference "${ref}" (at 0 0.8 ${rot}) (layer "F.SilkS") (effects (font (size 0.4 0.4) (thickness 0.08))))
+      (pad "1" smd rect (at -0.5 0) (size 0.6 0.6) (layers "F.Cu" "F.Paste" "F.Mask") ${getNetStr(net1)} ${genTstamp()})
+      (pad "2" smd rect (at 0.5 0) (size 0.6 0.6) (layers "F.Cu" "F.Paste" "F.Mask") ${getNetStr(net2)} ${genTstamp()})
+    )\n`;
+}
+
+function placeTagConnect(x, y, rot) {
+  return `(module TC2030-IDC-NL:Tag-Connect_TC2030-IDC-NL_2x03_P1.27mm_Vertical (layer "Front") (at ${x} ${y} ${rot}) (tstamp ${Math.floor(Math.random() * 1000000)})
+      (fp_text reference "J1" (at 0 2.5 ${rot}) (layer "F.SilkS") (effects (font (size 0.5 0.5) (thickness 0.1))))
+      (pad "1" smd circle (at -0.635 1.27) (size 0.787 0.787) (layers "F.Cu" "F.Paste" "F.Mask") ${getNetStr("+3V")} ${genTstamp()})
+      (pad "2" smd circle (at 0.635 1.27) (size 0.787 0.787) (layers "F.Cu" "F.Paste" "F.Mask") ${getNetStr("SWIM")} ${genTstamp()})
+      (pad "3" smd circle (at -0.635 0) (size 0.787 0.787) (layers "F.Cu" "F.Paste" "F.Mask") ${getNetStr("GND")} ${genTstamp()})
+      (pad "4" smd circle (at 0.635 0) (size 0.787 0.787) (layers "F.Cu" "F.Paste" "F.Mask") ${getNetStr("RST")} ${genTstamp()})
+      (pad "5" smd circle (at -0.635 -1.27) (size 0.787 0.787) (layers "F.Cu" "F.Paste" "F.Mask") ${getNetStr("TX")} ${genTstamp()})
+      (pad "6" smd circle (at 0.635 -1.27) (size 0.787 0.787) (layers "F.Cu" "F.Paste" "F.Mask") ${getNetStr("RX")} ${genTstamp()})
+    )\n`;
+}
+
 function placeHook(x, y, rot) {
-  return `(module earringHookWire:earringHookWire (layer "Front") (at ${x} ${y} ${rot}) (tstamp ${Math.floor(Math.random() * 1000000)})
-      (fp_text reference "H1" (at 0 -5) (layer "F.SilkS") (effects (font (size 1 1) (thickness 0.15))))
+  return `(module tooling:TestPoint_Pad_D4.0mm (layer "Front") (at ${x} ${y} ${rot}) (tstamp ${Math.floor(Math.random() * 1000000)})
+      (fp_text reference "H1" (at 0 -4) (layer "F.SilkS") (effects (font (size 0.8 0.8) (thickness 0.15))))
+      (pad "1" thru_hole circle (at 0 0) (size 3 3) (drill 2) (layers "*.Cu" "*.Mask") ${genTstamp()})
     )\n`;
 }
 
@@ -188,9 +250,19 @@ function header() {
 
 // Generate File
 let pcbFile = header();
-pcbFile += placeUc(0, 0, 0);
-pcbFile += placeHook(0, -13, 0);
-pcbFile += placeBattery(1, 0, 0, 0);
+pcbFile += placeUc(0, -4, 90);
+pcbFile += placeButton(0, 0, 180);
+pcbFile += placeMic(3.475, 3.535, 0);
+pcbFile += placeTagConnect(-4.465, -1.85, -90);
+pcbFile += placeHook(0, -13.5, 0);
+pcbFile += placeBattery(0, 0, 0);
+
+// Passives
+pcbFile += placePassive("C1", "1uF", "C-0402", -5.65, 2.2, 90, "+3V", "GND");
+pcbFile += placePassive("C2", "1uF", "C-0402", 3.95, 1.05, 90, "+3V", "GND");
+pcbFile += placePassive("R3", "10k", "R-0402", -3.3, 5.15, 0, "+3V", "RST");
+pcbFile += placePassive("R1", "100", "R-0402", -1.5, -6.5, 0, "MIC", "CPX-0"); // Dummy/Approx
+pcbFile += placePassive("R2", "100", "R-0402", 1.5, -6.5, 0, "MIC_PWR", "CPX-1"); // Dummy/Approx
 
 // Outline
 pcbFile += `(gr_arc (start 0 12) (mid 11.5 2) (end 4.1 -11.3) (stroke (width 0.05) (type solid)) (layer "Edge.Cuts") (tstamp ${Math.floor(Math.random() * 1000000)}))\n`;
